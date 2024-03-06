283ba06c736404a8c42c1c88b5fc0ca68d4085cc
justmero02 <162280273+justmero02@users.noreply.github.com>
2024-03-06 11:31:59 +0300

    Create mericyanik.java

---
+++ JavaPractices/mericyanik.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class Java_Test {
+
+    public static void main(String[] args) {
+        
+        int a = 5, b = 6, toplam;
+        toplam = a + b;
+        System.out.println("Toplam: " + toplam);
+    }
+}
